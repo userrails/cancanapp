8a0f760c0b48bbfbfadc9a1d073c38848d0c9fe7
rails <rails@rails>
2015-05-09 13:37:44 +0530

    Basic structure completed working on cancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    user
+    @joined_on = @user.created_at.to_formatted_s(:short)
+    if @user.current_sign_in_at
+      @last_login = @user.current_sign_in_at.to_formatted_s(:short)
+    else
+      @last_login = "never"
+    end
+  end
+
+  private
+  def user
+    @user = User.find(params[:id])
+  end
 end
