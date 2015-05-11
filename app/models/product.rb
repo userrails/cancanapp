5a6f18f2eaa67f1170ec3b2fa8cb7df8a786b74e
rails <rails@rails>
2015-05-11 13:45:49 +0530

    Polymorphic assoc betn blog, product, comment

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   include ProductValidate
+  has_many :comments, as: :commentable #this is simple a name like as alias
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511071324) do
+ActiveRecord::Schema.define(version: 20150511073649) do
 
   create_table "blogs", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "body"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
