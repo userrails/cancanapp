d286510c0b9ac538a6c6578571c856f399f06b30
rails <rails@rails>
2015-05-11 12:54:35 +0530

    create blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511061238) do
+ActiveRecord::Schema.define(version: 20150511071324) do
+
+  create_table "blogs", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "items", force: true do |t|
     t.string   "name"
